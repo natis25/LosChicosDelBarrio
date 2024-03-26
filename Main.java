9080fade6b18a0b2e4aa785cca4d9672fa165406
natis25 <150816023+natis25@users.noreply.github.com>
2024-03-26 09:41:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner lector = new Scanner(System.in);
 		int numero = 0;
+		//Natalia Urrutia
 		if (numero >= 20 && numero <= 24) {
 			System.out.println("Nati U.");
+		//Daniela Oropeza
 		} else if (numero < 0) {
 			System.out.println("Dani Nicole.");
 		}

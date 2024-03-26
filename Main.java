67885d945572bbf33cfaff8fac105265bb56d7b5
DanielaOropezaBalcazar <164942966+DanielaOropezaBalcazar@users.noreply.github.com>
2024-03-26 10:02:27 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Ingrese un numero entero menor o igual a 50");
+		int numero = lector.nextInt();
+			//Daniela Oropeza
+			if (numero < 0) {
+				System.out.println("Dani Nicole.");
+			}
+}

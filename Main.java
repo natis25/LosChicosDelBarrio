850d4f03a27943188b0968ff7c3f6de9acee18fb
natis25 <150816023+natis25@users.noreply.github.com>
2024-03-26 09:46:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner lector = new Scanner(System.in);
-		System.out.prinln("Ingrese un numero entero");
+		System.out.println("Ingrese un numero entero");
 		int numero = lector.nextInt();
 		//Natalia Urrutia
 		if (numero >= 20 && numero <= 24) {
